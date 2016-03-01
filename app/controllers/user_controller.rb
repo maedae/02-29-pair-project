7e71f70e5629bf157e6365077213330ffa68b84f
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-01 17:49:31 -0600

    can now delete user account. requires password confirmation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -23,47 +23,63 @@ MyApp.get "/home" do
 
           if @current_buildings.empty?
              @no_current_building_error = true
-           end
+          end
       end
       erb :"/users/view_user_dashboard"
     end
 end
 
-MyApp.get "/users/:id/authenticate" do
+MyApp.get "/users/update/authenticate" do
   @current_user = User.find_by_id(session[:user_id])
-  erb :"/users/user_authentication"
+  erb :"/users/update_user_authentication"
 end 
 
-MyApp.post "/users/:id/authenticate/confirmation" do
+MyApp.post "/users/update/authenticate/confirmation" do
   @current_user = User.find_by_id(session[:user_id])
     if @current_user.password != params[:authentication]
       @authentication_error = true
-      erb :"/users/user_authentication"
+      erb :"/users/update_user_authentication"
     else
-      redirect :"/users/#{session[:user_id]}/update"
+      redirect :"/users/update"
     end
 end 
 
-MyApp.get "/users/:id/update" do
+MyApp.get "/users/update" do
   @current_user = User.find_by_id(session[:user_id])
   erb :"/users/update_user"
 end 
 
 MyApp.post "/users/update/confirmation" do
-     @current_user = User.find_by_id(session[:user_id])
-     @current_user.name = params[:name].downcase.capitalize
-     @current_user.email = params[:email]
-     @current_user.password = params[:password]
-     @error_check = @current_user.create_user_check_valid_action
+ @current_user = User.find_by_id(session[:user_id])
+ @current_user.name = params[:name].downcase.capitalize
+ @current_user.email = params[:email]
+ @current_user.password = params[:password]
+ @error_check = @current_user.create_user_check_valid_action
 
-      if @error_check.empty? == false
-          @error = true
-          erb :"/users/update_user"
-      elsif User.where({"email" => @current_user.email}).where.not("id" => @current_user.id).length >= 1
-          @duplicate_email_error = true
-          erb :"/users/update_user"
-      else
-        @current_user.save
-        redirect :"/home"
-      end
+  if @error_check.empty? == false
+      @error = true
+      erb :"/users/update_user"
+  elsif User.where({"email" => @current_user.email}).where.not("id" => @current_user.id).length >= 1
+      @duplicate_email_error = true
+      erb :"/users/update_user"
+  else
+    @current_user.save
+    redirect :"/home"
+  end
+end
+
+MyApp.get "/users/delete/authenticate" do
+  erb :"/users/delete_user_authentication"
+end
+
+MyApp.post "/users/delete/authenticate/confirmation" do
+  @current_user = User.find_by_id(session[:user_id])
+  if @current_user.password != params[:authentication]
+    @authentication_error = true
+    erb :"/users/delete_user_authentication"
+  else
+    @current_user.delete
+    session[:user_id] = nil
+    redirect :"/login"
+  end
 end
