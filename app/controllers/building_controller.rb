5848b6d153b9fb83d53b16c476e375b8f51bd4b3
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-01 14:00:34 -0600

    added hyperlink to past rentals page

--- app/controllers/building_controller.rb
+++ app/controllers/building_controller.rb
@@ -4,3 +4,17 @@ MyApp.before "/buildings*" do
         redirect :"/login"
     end
 end
+
+MyApp.get "/buildings/history" do 
+    @current_user = User.find_by_id(session[:user_id])
+    @rent_info = @current_user.find_user_renter_building_info
+    if @rent_info.empty? == true
+      @no_renter_info_error = true
+    else
+      @past_buildings = @current_user.past_building_info_for_renter_based_on_user_id
+        if @past_buildings.empty?
+          @no_previous_building_error = true
+        end
+    end
+    erb :"/buildings/view_past_buildings"
+end
