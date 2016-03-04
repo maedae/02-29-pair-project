997f68d9281237fbaa57ecb1dfa3df982ae12c6d
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-04 13:31:05 -0600

    fixed pathways for demo

--- tests/unit/building_test.rb
+++ tests/unit/building_test.rb
@@ -6,25 +6,22 @@ class BuildingTest < Minitest::Test
     MyApp
   end
 
-  def setup
+def setup
     super
 
     @current_user = User.new
-    @current_user.id = 1
     @current_user.name = ""
     @current_user.email = "www.test@email.com"
     @current_user.password = ""
     @current_user.save
 
     @other_user = User.new
-    @other_user.id = 2
     @other_user.name = "bob"
     @other_user.email = ""
     @other_user.password = "password"
     @other_user.save
 
     @current_building = Building.new
-    @current_building.id = 1
     @current_building.address = ""
     @current_building.apt_no = ""
     @current_building.city = "Some Town"
@@ -35,12 +32,11 @@ class BuildingTest < Minitest::Test
     @current_building.move_in = ""
     @current_building.move_out = "2001-01-01"
     @current_building.locked = false
-    @current_building.created_by = 1
-    @current_building.updated_by = 1
+    @current_building.created_by = @current_user.id
+    @current_building.updated_by = @current_user.id
     @current_building.save
 
     @past_building = Building.new
-    @past_building.id = 2
     @past_building.address = "1414 14th"
     @past_building.apt_no = ""
     @past_building.city = "Some City"
@@ -51,43 +47,107 @@ class BuildingTest < Minitest::Test
     @past_building.move_in = "1999-01-01"
     @past_building.move_out = "2000-01-01"
     @past_building.locked = true
-    @past_building.created_by = 1
-    @past_building.updated_by = 1
+    @past_building.created_by = @other_user.id
+    @past_building.updated_by = @other_user.id
     @past_building.save
 
+    @abandoned_building = Building.new
+    @abandoned_building.address = ""
+    @abandoned_building.apt_no = ""
+    @abandoned_building.city = "Some Town"
+    @abandoned_building.state = "Nevada"
+    @abandoned_building.zip_code = "68046"
+    @abandoned_building.landlord_name = "Ethel"
+    @abandoned_building.building_image = "www.someimage.com"
+    @abandoned_building.move_in = ""
+    @abandoned_building.move_out = "2001-01-01"
+    @abandoned_building.locked = false
+    @abandoned_building.created_by = @current_user.id
+    @abandoned_building.updated_by = @current_user.id
+    @abandoned_building.save
+
     @new_room = Room.new
-    @new_room.id = 1
     @new_room.title = "bedroom"
-    @new_room.building_id = 1
+    @new_room.building_id = @current_building.id
     @new_room.save
 
+    @other_room = Room.new
+    @other_room.title = ""
+    @other_room.building_id = @past_building.id
+    @other_room.save
+
+
+    @new_item = Item.new
+    @new_item.title = "broken window"
+    @new_item.room_id = @new_room.id
+    @new_item.description = "Window sucks"
+    @new_item.condition = 2
+    @new_item.created_by = @current_user.id
+    @new_item.updated_by = @current_user.id 
+    @new_item.save
+
+    @historic_item = Item.new
+    @historic_item.title = ""
+    @historic_item.room_id = @new_room.id
+    @historic_item.description = ""
+    @historic_item.condition = 5
+    @historic_item.created_by = @current_user.id
+    @historic_item.updated_by = @current_user.id
+    @historic_item.save
+
+    @other_item = Item.new
+    @other_item.title = "broken cat"
+    @other_item.room_id = @other_room.id
+    @other_item.description = ""
+    @other_item.condition = nil
+    @other_item.created_by = @current_user.id
+    @other_item.updated_by = @current_user.id
+    @other_item.save
+
+
     @renter_1 = Renter.new
-    @renter_1.id = 1
-    @renter_1.user_id = 1
-    @renter_1.building_id = 1
+    @renter_1.user_id = @current_user.id
+    @renter_1.building_id = @current_building.id
     @renter_1.save
 
     @renter_2 = Renter.new
-    @renter_2.id = 2
-    @renter_2.user_id = 2
-    @renter_2.building_id = 1
+    @renter_2.user_id = @other_user.id
+    @renter_2.building_id = @current_building.id
     @renter_2.save
 
     @renter_3 = Renter.new
-    @renter_3.id = 3
-    @renter_3.user_id = 1
-    @renter_3.building_id = 2
+    @renter_3.user_id = @current_user.id
+    @renter_3.building_id = @past_building.id
     @renter_3.save
 
-    
+    @photo_1 = Photo.new
+    @photo_1.item_id = @new_item.id
+    @photo_1.image = "yiwer"
+    @photo_1.save
 
-  end
+    @photo_3 = Photo.new
+    @photo_3.item_id = @new_item.id
+    @photo_3.image = "gvfujs"
+    @photo_3.save
+
+    @photo_2 = Photo.new
+    @photo_2.item_id = @other_item.id
+    @photo_2.image = "ubeablu"
+    @photo_2.save
+
+
+   
+end
 
 ## BUILDING TESTS - START
 
   def test_find_rooms_for_building
       assert_equal([1], @current_building.find_rooms_for_building)
       assert_equal(nil, @past_building.find_rooms_for_building)
+  end
+
+  def test_check_if_building_has_other_renters
+
   end
 
   def test_create_building_check_valid_action
