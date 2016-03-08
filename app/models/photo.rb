e7857e3fb045cafbb09cf953a01748706285f06b
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-07 20:42:40 -0600

    resolved irresponsible module error.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -8,4 +8,18 @@ class Photo < ActiveRecord::Base
     return "Please select an image to add."
   end
 
+  # Method searches all Items for objects associated with the Room,
+  # pushes them each into an array, then searches all Photos for 
+  # objects associated with each Item object. Method then deletes all
+  # associated Photo objects.
+  #
+  def find_and_delete_item_photos_for_room(item_id_arr)
+      item_id_arr. each do |item|
+        item_photo = Photo.where({"item_id" => item}) 
+        if item_photo != nil
+          item_photo.delete_all
+        end
+      end
+  end
+
 end
